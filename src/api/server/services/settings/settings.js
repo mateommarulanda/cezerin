399d858057a7acd23810fcda918ac5698cf40f42
Mateo Martinez Marulanda <mateomm01@gmail.com>
2024-06-12 09:30:24 -0500

    refactoring of the method getValidDocumentForUpdate, readability and complexity problems in the code are corrected

--- src/api/server/services/settings/settings.js
+++ src/api/server/services/settings/settings.js
@@ -77,113 +77,50 @@ class SettingsService {
 
 	getValidDocumentForUpdate(data) {
 		if (Object.keys(data).length === 0) {
-			return new Error('Required fields are missing');
+		  return new Error('Required fields are missing');
 		}
-
-		let settings = {};
-
-		if (data.language !== undefined) {
-			settings.language = parse.getString(data.language);
-		}
-
-		if (data.currency_code !== undefined) {
-			settings.currency_code = parse.getString(data.currency_code);
-		}
-
-		if (data.domain !== undefined) {
-			settings.domain = parse.getString(data.domain);
-		}
-
-		if (data.currency_symbol !== undefined) {
-			settings.currency_symbol = parse.getString(data.currency_symbol);
-		}
-
-		if (data.currency_format !== undefined) {
-			settings.currency_format = parse.getString(data.currency_format);
-		}
-
-		if (data.thousand_separator !== undefined) {
-			settings.thousand_separator = parse.getString(data.thousand_separator);
-		}
-
-		if (data.decimal_separator !== undefined) {
-			settings.decimal_separator = parse.getString(data.decimal_separator);
-		}
-
-		if (data.decimal_number !== undefined) {
-			settings.decimal_number =
-				parse.getNumberIfPositive(data.decimal_number) || 0;
-		}
-
-		if (data.timezone !== undefined) {
-			settings.timezone = parse.getString(data.timezone);
-		}
-
-		if (data.date_format !== undefined) {
-			settings.date_format = parse.getString(data.date_format);
-		}
-
-		if (data.time_format !== undefined) {
-			settings.time_format = parse.getString(data.time_format);
-		}
-
-		if (data.default_shipping_country !== undefined) {
-			settings.default_shipping_country = parse.getString(
-				data.default_shipping_country
-			);
-		}
-
-		if (data.default_shipping_state !== undefined) {
-			settings.default_shipping_state = parse.getString(
-				data.default_shipping_state
-			);
-		}
-
-		if (data.default_shipping_city !== undefined) {
-			settings.default_shipping_city = parse.getString(
-				data.default_shipping_city
-			);
-		}
-
-		if (data.default_product_sorting !== undefined) {
-			settings.default_product_sorting = parse.getString(
-				data.default_product_sorting
-			);
-		}
-
-		if (data.product_fields !== undefined) {
-			settings.product_fields = parse.getString(data.product_fields);
-		}
-
-		if (data.products_limit !== undefined) {
-			settings.products_limit = parse.getNumberIfPositive(data.products_limit);
-		}
-
-		if (data.weight_unit !== undefined) {
-			settings.weight_unit = parse.getString(data.weight_unit);
-		}
-
-		if (data.length_unit !== undefined) {
-			settings.length_unit = parse.getString(data.length_unit);
-		}
-
-		if (data.logo_file !== undefined) {
-			settings.logo_file = parse.getString(data.logo_file);
-		}
-
-		if (data.hide_billing_address !== undefined) {
-			settings.hide_billing_address = parse.getBooleanIfValid(
-				data.hide_billing_address,
-				false
-			);
-		}
-
-		if (data.order_confirmation_copy_to !== undefined) {
-			settings.order_confirmation_copy_to = parse.getString(
-				data.order_confirmation_copy_to
-			);
-		}
-
+	  
+		const settings = {};
+	  
+		Object.keys(this.defaultSettings).forEach(key => {
+		  if (data[key]!== undefined) {
+			switch (key) {
+			  case 'language':
+			  case 'currency_code':
+			  case 'domain':
+			  case 'currency_symbol':
+			  case 'currency_format':
+			  case 'thousand_separator':
+			  case 'decimal_separator':
+			  case 'timezone':
+			  case 'date_format':
+			  case 'time_format':
+			  case 'default_shipping_country':
+			  case 'default_shipping_state':
+			  case 'default_shipping_city':
+			  case 'default_product_sorting':
+			  case 'product_fields':
+			  case 'weight_unit':
+			  case 'length_unit':
+			  case 'logo_file':
+			  case 'order_confirmation_copy_to':
+				settings[key] = parse.getString(data[key]);
+				break;
+			  case 'decimal_number':
+				settings[key] = parse.getNumberIfPositive(data[key]) || 0;
+				break;
+			  case 'products_limit':
+				settings[key] = parse.getNumberIfPositive(data[key]);
+				break;
+			  case 'hide_billing_address':
+				settings[key] = parse.getBooleanIfValid(data[key], false);
+				break;
+			  default:
+				throw new Error(`Unknown setting: ${key}`);
+			}
+		  }
+		});
+	  
 		return settings;
 	}
 
